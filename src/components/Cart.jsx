a91e853473da496639a7ca127d4510a1271a7884
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-07-24 15:27:14 +0530

    cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem'
 
 const Cart = () => {
+  const {cart} = useSelector((state) => state)
   return (
-    <></>
+    <>
+    <div>
+      {
+        cart.length > 0 ? 
+        ( <div> 
+            {
+              cart.map((item , index)=>(
+                <CartItem key={item.id} item={item} itemIndex={index} />
+              ))
+            }
+          </div> ) :
+        ( <div className='empty '>No item in your cart</div> )
+      }
+    </div>
+    </>
   )
 }
 
